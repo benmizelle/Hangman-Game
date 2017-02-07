79e408f1d0527e15db03b1fa5677e61afc186427
Ben Mizelle <Ben Mizelle>
2017-02-06 22:18:35 -0500

    begin javascript

---
+++ assets/javascript/game.js
@@ -0,0 +1,5 @@
+var words  = ["Mario", "Luigi", "Peach", "Bowser", "Yoshi", "Zelda", "Link", "Pikachu", "Pokemon", "Metroid"];
+	console.log(words.length);
+
+var rand = words[Math.floor(Math.random() * words.length)];
+	console.log(rand);
