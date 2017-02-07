bc627c2bb00bd2790633e175010e31a7ced279f7
Ben Mizelle <Ben Mizelle>
2017-02-06 23:16:21 -0500

    altered styling, worked on javascript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,16 @@
 var words  = ["Mario", "Luigi", "Peach", "Bowser", "Yoshi", "Zelda", "Link", "Pikachu", "Pokemon", "Metroid"];
 	console.log(words.length);
 
-var rand = words[Math.floor(Math.random() * words.length)];
-	console.log(rand);
+var answerWord = words[Math.floor(Math.random() * words.length)];
+	console.log(answerWord);
+
+function blanksFromAnswer ( answerWord ) {
+    
+ 	var result = "words";
+    
+        for(var i = 0; i < words.length; i++) {
+        answerWord[i] = "_";
+    }
+    
+    return result;
+}
