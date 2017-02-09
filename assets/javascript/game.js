9d2bccc9fb0a61581ec9f1ca4439eb11a5b7fb76
Ben Mizelle <Ben Mizelle>
2017-02-08 21:19:28 -0500

    still working on javascript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,10 @@
+// Array of possible answers
 var words  = ["Mario", "Luigi", "Peach", "Bowser", "Yoshi", "Zelda", "Link", "Pikachu", "Pokemon", "Metroid"];
-
+// Chooses a random word from the array named words
 var answerWord = words[Math.floor(Math.random() * words.length)];
 	console.log(answerWord);
 var correctAnswer=[];
-
+// for loop: the characters of the answer word with the character: "_"
 for (var i=0; i < answerWord.length ; i++) {
     correctAnswer[i]= " _ ";
 }
@@ -12,18 +13,35 @@ console.log(correctAnswer);
 
 document.getElementById("blankSpaces").innerHTML = correctAnswer.join(" ");
 
-  var guessesLeft = 10;
-  var losses = 0;
-  var wins = 0;
-  var previousGuesses = [];
-  var letterGuessed = [];
 
+
+
+var guessesLeft = 10;
+
+var losses = 0;
+
+var wins = 0;
+
+var previousGuesses = [];
+
+// Listen for key press, then...
  document.onkeyup=function(){
  	var letterGuessed = String.fromCharCode(event.keyCode).toLowerCase;
  	console.log(letterGuessed);
 
+ 
+// Loop through letters of Randomly generated answer word
 for (var i = 0; i < answerWord.length; ++i) {
     var letterGuessed = answerWord.charAt(i);
     console.log(letterGuessed);
 }
+// if(letterGuess is not found in the previousGuesses array) {
+// add the letterGuess into the previousGuesses array
+if (letterGuessed !== previousGuesses); {
+     previousGuesses.push(letterGuessed);
+     console.log("new guess");
+// close if
+}
+
+// close onkeyup function
 }
